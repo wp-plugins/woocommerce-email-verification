0cb02df08134644d15ea18f6d78d0a7512b0bcc9
subhansanjaya <subhansanjaya@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-20 00:46:30 +0000

    Updating version 2.1.0
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-email-verification/tags/2.1.0@861303 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-email-verification.php
+++ woocommerce-email-verification.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: WooCommerce Email Verification
- * Version: 1.0.0
+ * Version: 2.1.0
  * Plugin URI: http://wordpress.org/plugins/woocommerce-email-verification/
  * Description: Sends a verification link on users mail ID to activate their account after register.
  * Author: subhansanjaya
